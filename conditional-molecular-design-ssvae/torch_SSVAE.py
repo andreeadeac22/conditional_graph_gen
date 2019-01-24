a780bebf890410410e4d7d5984afba8d95bc7ff1
andreeadeac22 <andreeadeac22@gmail.com>
2019-01-24 17:29:46 +0000

    RNNpredictor working

--- conditional-molecular-design-ssvae/torch_SSVAE.py
+++ conditional-molecular-design-ssvae/torch_SSVAE.py
@@ -1,20 +1,45 @@
 import torch
+import torch.nn as nn
 
-from constants import *
+from torch_constants import *
 
 class TorchSSVAE(nn.Module):
     def __init__(self, seqlen_x, dim_x, dim_y, dim_z=100, dim_h=250, n_hidden=3, batch_size=200, beta=10000, char_set=[' ']):
         super(TorchSSVAE, self).__init__()
         self.seqlen_x, self.dim_x, self.dim_y, self.dim_z, self.dim_h, self.n_hidden, self.batch_size = seqlen_x, dim_x, dim_y, dim_z, dim_h, n_hidden, batch_size
-         self.beta = beta
 
-         self.char_to_int = dict((c,i) for i,c in enumerate(char_set))
-         self.int_to_char = dict((i,c) for i,c in enumerate(char_set))
+        self.char_to_int = dict((c,i) for i,c in enumerate(char_set))
+        self.int_to_char = dict((i,c) for i,c in enumerate(char_set))
+
+        self.predi_gru = torch.nn.GRU(dim_x, dim_h, num_layers=n_hidden, batch_first=True, bidirectional=True)
+        self.predi_dense = torch.nn.Linear(dim_h * 2, dim_y)
+
+
 
     def forward(self, x_L, xs_L, y_L, x_U, xs_U):
-        
-        return cost, objL, objU, objYpred_MSE
+        zero_state = torch.zeros(self.n_hidden *2, x_L.shape[0], self.dim_h)
+        if torch.cuda.is_available():
+            zero_state = zero_state.cuda()
+        #print(x_L.shape)
 
+        _, final_state = self.predi_gru(x_L, zero_state)
+
+        #print("final_state", final_state.shape)
+
+        cat_fw_bw = torch.cat([final_state[-1,:,:], final_state[-2,:,:]], dim=1)
+
+        #print("cat_fw_bw.shape", cat_fw_bw.shape)
+
+        predi_res = self.predi_dense(cat_fw_bw)
+
+        return predi_res
+
+    """
     def rnnpredictor(self, x, dim_h, dim_y, reuse=False):
-        gru = torch.nn.GRU(dim_h, self.n_hidden, batch_first=True)
-        rnn = torch.nn.RNN(self.n_hidden, 1)
+        gru = torch.nn.GRU(x.shape[0], dim_h, num_layers=n_hidden, batch_first=True, bidirectional=True)
+        zero_state = torch.zeros(x.shape[1], x.shape[2])
+        _, final_state = gru(x, zero_state)
+        print("final_state", final_state.shape)
+        dense = torch.nn.Linear(final_state.shape[-1] * 2, dim_y)
+        return dense(torch.concat(final_state[0], final_state[1]))
+    """
--- conditional-molecular-design-ssvae/torch_run.py
+++ conditional-molecular-design-ssvae/torch_run.py
@@ -2,102 +2,101 @@ from __future__ import print_function
 
 import torch
 import numpy as np
-import pandas as pds
-from preprocessing import smiles_to_seq, vectorize
-#from torch_SSVAE import *
-
-from preprocessing import get_property, canonocalize
-from constants import *
-from util import *
+import pandas as pd
+from preprocessing import smiles_to_seq, vectorize, get_property, canonocalize
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error
+import torch.nn.functional as F
 
-
-
-# data preparation
-print('::: data preparation')
-
-smiles = pds.read_csv(data_uri).values[:ntrn+ntst,0] #0: SMILES
-Y = np.asarray(pds.read_csv(data_uri).values[:ntrn+ntst,1:], dtype=np.float32) # 1: MolWT, 2: LogP, 3: QED
-
-list_seq = smiles_to_seq(smiles, char_set)
-Xs, X=vectorize(list_seq, char_set)
-
-
-tstX=X[-ntst:]
-tstXs=Xs[-ntst:]
-tstY=Y[-ntst:]
-
-X=X[:ntrn]
-Xs=Xs[:ntrn]
-Y=Y[:ntrn]
-
-nL=int(len(Y)*frac)
-nU=len(Y)-nL
-nL_trn=int(nL*(1-frac_val))
-nL_val=nL-nL_trn
-nU_trn=int(nU*(1-frac_val))
-nU_val=nU-nU_trn
-perm_id=np.random.permutation(len(Y))
-
-trnX_L=X[perm_id[:nL_trn]]
-trnXs_L=Xs[perm_id[:nL_trn]]
-trnY_L=Y[perm_id[:nL_trn]]
-
-valX_L=X[perm_id[nL_trn:nL_trn+nL_val]]
-valXs_L=Xs[perm_id[nL_trn:nL_trn+nL_val]]
-valY_L=Y[perm_id[nL_trn:nL_trn+nL_val]]
-
-trnX_U=X[perm_id[nL_trn+nL_val:nL_trn+nL_val+nU_trn]]
-trnXs_U=Xs[perm_id[nL_trn+nL_val:nL_trn+nL_val+nU_trn]]
-
-valX_U=X[perm_id[nL_trn+nL_val+nU_trn:]]
-valXs_U=Xs[perm_id[nL_trn+nL_val+nU_trn:]]
-
-scaler_Y = StandardScaler()
-scaler_Y.fit(Y)
-trnY_L=scaler_Y.transform(trnY_L)
-valY_L=scaler_Y.transform(valY_L)
-
-#trnX_L (142500, 89, 36)
-#trnXs_L (142500, 89, 36)
-#trnY_L (142500, 3)
-#valX_L (7500, 89, 36)
-#valXs_L (7500, 89, 36)
-#valY_L (7500, 3)
-
-#trnX_U (142500, 89, 36)
-#trnXs_U (142500, 89, 36)
-#valX_U (7500, 89, 36)
-#valXs_U (7500, 89, 36)
-
-## model training
-print('::: model training')
-
-seqlen_x = X.shape[1]
-dim_x = X.shape[2]
-dim_y = Y.shape[1]
-
-batch_size_L=int(self.batch_size*len(trnX_L)/(len(trnX_L)+len(trnX_U)))
-batch_size_U=int(self.batch_size*len(trnX_U)/(len(trnX_L)+len(trnX_U)))
-n_batch=int(len(trnX_L)/batch_size_L)
-
-batch_size_val_L=int(len(valX_L)/10)
-batch_size_val_U=int(len(valX_U)/10)
-
+from torch_SSVAE import *
+from util import *
+from torch_constants import *
 
 def run():
+    # data preparation
+    print('::: data preparation')
+
+    smiles = pd.read_csv(data_uri).values[:ntrn+ntst,0] #0: SMILES
+    Y = np.asarray(pd.read_csv(data_uri).values[:ntrn+ntst,1:], dtype=np.float32) # 1: MolWT, 2: LogP, 3: QED
+
+    list_seq = smiles_to_seq(smiles, char_set)
+    Xs, X=vectorize(list_seq, char_set)
+
+
+    tstX=X[-ntst:]
+    tstXs=Xs[-ntst:]
+    tstY=Y[-ntst:]
+
+    X=X[:ntrn]
+    Xs=Xs[:ntrn]
+    Y=Y[:ntrn]
+
+    nL=int(len(Y)*frac)
+    nU=len(Y)-nL
+    nL_trn=int(nL*(1-frac_val))
+    nL_val=nL-nL_trn
+    nU_trn=int(nU*(1-frac_val))
+    nU_val=nU-nU_trn
+    perm_id=np.random.permutation(len(Y))
+
+    trnX_L=X[perm_id[:nL_trn]]
+    trnXs_L=Xs[perm_id[:nL_trn]]
+    trnY_L=Y[perm_id[:nL_trn]]
+
+    valX_L=X[perm_id[nL_trn:nL_trn+nL_val]]
+    valXs_L=Xs[perm_id[nL_trn:nL_trn+nL_val]]
+    valY_L=Y[perm_id[nL_trn:nL_trn+nL_val]]
+
+    trnX_U=X[perm_id[nL_trn+nL_val:nL_trn+nL_val+nU_trn]]
+    trnXs_U=Xs[perm_id[nL_trn+nL_val:nL_trn+nL_val+nU_trn]]
+
+    valX_U=X[perm_id[nL_trn+nL_val+nU_trn:]]
+    valXs_U=Xs[perm_id[nL_trn+nL_val+nU_trn:]]
+
+    scaler_Y = StandardScaler()
+    scaler_Y.fit(Y)
+    trnY_L=scaler_Y.transform(trnY_L)
+    valY_L=scaler_Y.transform(valY_L)
+
+    #trnX_L (142500, 89, 36)
+    #trnXs_L (142500, 89, 36)
+    #trnY_L (142500, 3)
+    #valX_L (7500, 89, 36)
+    #valXs_L (7500, 89, 36)
+    #valY_L (7500, 3)
+
+    #trnX_U (142500, 89, 36)
+    #trnXs_U (142500, 89, 36)
+    #valX_U (7500, 89, 36)
+    #valXs_U (7500, 89, 36)
+
+    ## model training
+    print('::: model training')
+
+    seqlen_x = X.shape[1]
+    dim_x = X.shape[2]
+    dim_y = Y.shape[1]
+
+    batch_size_L=int(batch_size*len(trnX_L)/(len(trnX_L)+len(trnX_U)))
+    batch_size_U=int(batch_size*len(trnX_U)/(len(trnX_L)+len(trnX_U)))
+    n_batch=int(len(trnX_L)/batch_size_L)
+
+    batch_size_val_L=int(len(valX_L)/10)
+    batch_size_val_U=int(len(valX_U)/10)
+
     mu_prior=torch.tensor(np.mean(trnY_L,0), dtype=torch.float32)
     cov_prior=torch.tensor(np.cov(trnY_L.T), dtype=torch.float32)
 
     model = TorchSSVAE(seqlen_x = seqlen_x, dim_x = dim_x, dim_y = dim_y, dim_z = dim_z, dim_h = dim_h, n_hidden = n_hidden, batch_size = batch_size, beta = float(beta), char_set = char_set)
-    optimizer = torch.optim.Adam(
-		model.parameters(), lr=learning_rate, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters())
 
-    for epoch in range(300):
+    if torch.cuda.is_available():
+        model.cuda()
+
+    for epoch in range(20):
         model.train()
-        [trnX_L, trnXs_L, trnY_L]= _permutation([trnX_L, trnXs_L, trnY_L])
-        [trnX_U, trnXs_U]= _permutation([trnX_U, trnXs_U])
+        [trnX_L, trnXs_L, trnY_L]= permutation([trnX_L, trnXs_L, trnY_L])
+        [trnX_U, trnXs_U]= permutation([trnX_U, trnXs_U])
 
 
         for i in range(n_batch):
@@ -107,22 +106,34 @@ def run():
             start_U=i*batch_size_U
             end_U=start_U+batch_size_U
 
+            x_L =  torch.tensor(trnX_L[start_L:end_L], dtype=torch.float32)
+            xs_L = torch.tensor(trnXs_L[start_L:end_L], dtype=torch.float32)
+            y_L = torch.tensor(trnY_L[start_L:end_L], dtype=torch.float32)
+            x_U =  torch.tensor(trnX_U[start_U:end_U], dtype=torch.float32)
+            xs_U =  torch.tensor(trnXs_U[start_U:end_U], dtype=torch.float32)
 
-            train_cost, train_objL, train_objU, train_objYpred_MSE = \
-                model(x_L =  torch.tensor(trnX_L[start_L:end_L], dtype=torch.float32),
-                      xs_L = torch.tensor(trnXs_L[start_L:end_L], dtype=torch.float32),
-                      y_L = torch.tensor(trnY_L[start_L:end_L], dtype=torch.float32),
-                      x_U =  torch.tensor(trnX_U[start_U:end_U], dtype=torch.float32),
-                      xs_U =  torch.tensor(trnXs_U[start_U:end_U], dtype=torch.float32))
+            if torch.cuda.is_available():
+                x_L = x_L.cuda()
+                xs_L = xs_L.cuda()
+                y_L = y_L.cuda()
+                x_U = x_U.cuda()
+                xs_U = xs_U.cuda()
+
+
+            predictions = model(x_L, xs_L, y_L, x_U, xs_U)
 
             # complete loss, equation 3
-            loss = (train_objL * float(batch_size_L) + train_objU * float(batch_size_U))/float(batch_size_L+batch_size_U) + float(batch_size_L)/float(batch_size_L+batch_size_U) * (beta * train_objYpred_MSE)
+            #loss = (train_objL * float(batch_size_L) + train_objU * float(batch_size_U))/float(batch_size_L+batch_size_U) + float(batch_size_L)/float(batch_size_L+batch_size_U) * (beta * train_objYpred_MSE)
+            criterion =  nn.MSELoss()
+            loss = criterion(predictions, y_L)
 
             model.zero_grad()
-            start = time.time()
+            #start = time.time()
             loss.backward()
             optimizer.step()
-            total_time += time.time() - start
+            #total_time += time.time() - start
+
+            #print("loss", loss)
 
 
         model.eval()
@@ -134,14 +145,28 @@ def run():
             start_U=i*batch_size_val_U
             end_U=start_U+batch_size_val_U
 
-            val_cost, val_objL, val_objU, val_objYpred_MSE =
-                model( x_L =  torch.tensor(trnX_L[start_L:end_L], dtype=torch.float32),
-                  xs_L = torch.tensor(trnXs_L[start_L:end_L], dtype=torch.float32),
-                  y_L = torch.tensor(trnY_L[start_L:end_L], dtype=torch.float32),
-                  x_U =  torch.tensor(trnX_U[start_U:end_U], dtype=torch.float32),
-                  xs_U =  torch.tensor(trnXs_U[start_U:end_U], dtype=torch.float32)))
+            #val_cost, val_objL, val_objU, val_objYpred_MSE = \'
+            x_L =  torch.tensor(valX_L[start_L:end_L], dtype=torch.float32)
+            xs_L = torch.tensor(valXs_L[start_L:end_L], dtype=torch.float32)
+            y_L = torch.tensor(valY_L[start_L:end_L], dtype=torch.float32)
+            x_U =  torch.tensor(valX_U[start_U:end_U], dtype=torch.float32)
+            xs_U =  torch.tensor(valXs_U[start_U:end_U], dtype=torch.float32)
 
-            val_res.append([val_cost, val_objL, val_objU, val_objYpred_MSE])
+            if torch.cuda.is_available():
+                x_L = x_L.cuda()
+                xs_L = xs_L.cuda()
+                y_L = y_L.cuda()
+                x_U = x_U.cuda()
+                xs_U = xs_U.cuda()
+
+            val_predictions = model(x_L, xs_L, y_L, x_U, xs_U)
+            # val_res.append([val_cost, val_objL, val_objU, val_objYpred_MSE])
+            val_loss = criterion(val_predictions, y_L)
+            print("val_loss", loss)
+
+
+            """
+            val_res.append([val_loss])
 
             val_res=np.mean(val_res,axis=0)
             print(epoch, ['Training', 'cost_trn', trn_res[1]])
@@ -151,17 +176,7 @@ def run():
             if epoch > 20 and np.min(val_log[0:epoch-10]) * 0.99 < np.min(val_log[epoch-10:epoch+1]):
                 print('---termination condition is met')
                 break
-
-
-
-
-
-
-
-
-
-
-
+            """
 
 
 if __name__ == '__main__':
